538c798e71476019d444d6c74d11aff2ec0aa41b
Darryl Kilzer Jr <darrylkilzer@gmail.com>
2019-11-20 20:27:25 -0700

    Update Session.js

--- app/Models/Session.js
+++ app/Models/Session.js
@@ -2,10 +2,9 @@ import Speaker from "./Speaker.js";
 import { generateId } from "../Utils.js";
 
 export default class Session {
-  constructor({ id = generateId(), name, speakers, time }) {
+  constructor({ id = generateId(), name, speakers }) {
     this.id = id;
     this.name = name;
     this.speakers = speakers.map(s => new Speaker(s));
-    this.time = time;
   }
 }
